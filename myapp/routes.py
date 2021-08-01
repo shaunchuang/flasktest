5ccdb41d0d4e152ca0b36ff1f307fc77a047f66e
shaunchuang <72535297+shaunchuang@users.noreply.github.com>
2021-08-01 14:30:02 +0800

    update:button

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,12 +1,17 @@
 # Home page route
-from flask import render_template
+from flask import render_template, request
 from myapp import app
 
-@app.route('/')
-@app.route('/index')
-def index():
-    course = {'id':'BDSE'}
-    lessons = [{'code':'J001','name':'Java Programming'},
-                {'code':'D002','name':'Data Analysis using Python'}]
-    return render_template('index.html',title='Group2',course=course, lessons=lessons)
 
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    course = {'id': 'BDSE'}
+    lessons = [{'code': 'J001', 'name': 'Java Programming'},
+               {'code': 'D002', 'name': 'Data Analysis using Python'}]
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'do':
+            return render_template('button2.html')
+        elif request.form['submit_button'] == 'nomore':
+            return render_template('button1.html')
+    return render_template('index.html', title='Group2', course=course, lessons=lessons)
